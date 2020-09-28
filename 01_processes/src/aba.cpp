febea9c9a75f197ed0bb1a123eba15dd53c123c6
Raphael Futschik <hapich@gmx.net>
2020-09-28 22:09:18 +0200

    Processes are sleeping correctly

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <unistd.h>
 #include <cstdlib>
+#include <chrono>
 
 using namespace std;
 
@@ -9,15 +10,21 @@ using namespace std;
 
 int main() {
 
+    std::chrono::milliseconds sleeptime(500);
     auto pid{fork()};
+    cout << "Hi" << endl;
     if (pid == 0) {
         while (true){
-            cout << "A";
+            cout << "A" << flush;
+            std::this_thread::sleep_for(sleeptime);
         }
     } else {
         while (true){
-            cout << "B";
+            cout << "B" << flush;
+            std::this_thread::sleep_for(sleeptime);
         }
     }
+
+    cout << "Bye" << endl;
     
 }
