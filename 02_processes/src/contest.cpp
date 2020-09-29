c090590f67371a6c2891680ed98df0e7981d7cc2
Raphael Futschik <hapich@gmx.net>
2020-09-29 13:38:38 +0200

    Threads return their total time

--- 02_processes/src/contest.cpp
+++ 02_processes/src/contest.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <string>
 #include <random>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ using namespace std;
 class Car{
     public:
     string name;
+    float gesTime{0};
 
     Car(string name){
         this->name = name;
@@ -19,34 +21,79 @@ class Car{
         int count{1};
         Car car{name};
         float seconds{};
-
+        float gesTime{};
         random_device rd;
         mt19937 gen{rd()};
-        uniform_real_distribution<> dis{1, 10};
+        uniform_real_distribution<> dis{1, 3};
 
 
-        while (true) {
-            seconds = dis(gen) * 1000.0;
-            cout << to_string(count) + " " + this->name + "mit " + to_string(seconds / 1000.0) + " Sekunden" << endl;
-
+        while (count <= 10) {
+            seconds = dis(gen);
+            gesTime += seconds;
+            seconds *= 1000.0;
+            
             this_thread::sleep_for(chrono::milliseconds((int)seconds));
+
+            cout << to_string(count) + " " + this->name + " mit "  << setprecision(3) << (seconds / 1000.0) << " Sekunden" << endl;
 
             count++;
         }
+
+        this->gesTime += gesTime;
+
+    
+    }
+
+    float getTime(){
+        return this->gesTime;
     }
 };
 
 
+void startRace(string name, float &gesTime) {
+    int count{1};
+    Car car{name};
+    float seconds{};
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{1, 3};
+
+
+    while (count <= 10) {
+        seconds = dis(gen);
+        gesTime += seconds;
+        seconds *= 1000.0;
+            
+        this_thread::sleep_for(chrono::milliseconds((int)seconds));
+
+        cout << to_string(count) + " " + name + " mit "  << setprecision(3) << (seconds / 1000.0) << " Sekunden" << endl;
+
+        count++;
+    }
+
+
+
+
+    
+    }
+
 
 
 int main() {
-    thread t{Car("Porsche")};
-    thread t2{Car("VW")};
 
-    
+    float gesTime{};
 
+    Car porsche{Car("Porsche")};
+
+    thread t{ref(porsche)};
+    thread t2{startRace, "VW", ref(gesTime)};
 
     t.join();
     t2.join();
+
+
+    cout << porsche.getTime() << endl;
+    cout << gesTime << endl;
+    
 
 }
