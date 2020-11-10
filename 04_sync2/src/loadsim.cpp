3eec9c02c09afb151aeda8e1acecc8efd0e80857
Raphael Futschik <futschik.raphael@student.htlwrn.ac.at>
2020-11-10 22:54:38 +0100

    worker loop works

--- 04_sync2/src/loadsim.cpp
+++ 04_sync2/src/loadsim.cpp
@@ -3,7 +3,9 @@
 #include <string>
 #include <queue>
 #include <chrono>
+#include <random>
 #include <sstream>
+#include <iomanip>
 
 #include "work_packet.h"
 #include "work_queue.h"
@@ -15,34 +17,54 @@ using namespace std;
 void worker(int id, WorkQueue& q){
     ostringstream buf;
     
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{1, 10};
+    float seconds {};
+
     while (true) {
-        
+        seconds = dis(gen);
+        seconds *= 1000;
+
         buf << "W" << id <<" : Want work packet" << endl;
         cout << buf.str() << flush;
         buf.str("");
-        
+
         WorkPacket processedPacket {q.pop()};
         buf << "W" << id <<" : Got work packet " << processedPacket.get_id() << endl;
         cout << buf.str() << flush;
         buf.str("");
-        this_thread::sleep_for(chrono::milliseconds(1000));
+        this_thread::sleep_for(chrono::milliseconds(int(seconds)));
         
-        buf << "W" << id <<" : Processed work packet " << processedPacket.get_id() << endl;
+        buf << "W" << id <<" : Processed work packet " << processedPacket.get_id() << " (" << setprecision(2) << (seconds / 1000.0) << ")" << endl;
         cout << buf.str() << flush;
         buf.str("");
         
     }
 }
 
+
+
 void submitting(WorkQueue& q){
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{0, 1};
+    float seconds {};
+
     int i {};
     ostringstream buf;
     while (true){
-        q.push(WorkPacket(i));
-        buf << "B : Submitted work packet " << i << endl;
+        seconds = dis(gen);
+        seconds *= 1000;
+        buf << "B : Waiting to submit work packet " << i << endl;
         cout << buf.str() << flush;
         buf.str("");
-        this_thread::sleep_for(chrono::milliseconds(500));
+        this_thread::sleep_for(chrono::milliseconds((int)seconds));
+        q.push(WorkPacket(i));
+        buf << "B : Submitted work packet " << i << " (" << setprecision(2) << (seconds / 1000.0) << ")" << endl;
+        cout << buf.str() << flush;
+        buf.str("");
+        
         i++;
     }
 }
